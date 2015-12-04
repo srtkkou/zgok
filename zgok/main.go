3d3e0547115101be341ad9cc26fafe79c1761a01
srtkkou <capibara.zoo@gmail.com>
2015-12-04 15:10:51 +0900

    Fix Fixed main command to use sub commands "build" and "show".

--- zgok/main.go
+++ zgok/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"github.com/srtkkou/zgok"
@@ -27,99 +26,93 @@ func (s *strSlice) Set(v string) error {
 	return nil
 }
 
-// Arguments.
-type Arguments struct {
-	isHelp    bool
-	isVersion bool
-	exePath   string
-	zipPaths  strSlice
-	outPath   string
-}
-
-// Set usage.
-func (a *Arguments) setUsage() {
-	flag.Usage = func() {
-		fmt.Println(zgok.Version())
-		fmt.Println()
-		fmt.Printf("Usage: %s [-h] [-v]\n", zgok.APP)
-		fmt.Printf("  -e exePath -z zipPath [-o outPath]\n")
-		fmt.Println()
-		fmt.Println("Options:")
-		fmt.Println("  -v : Print version.")
-		fmt.Println("  -h : Print this help message.")
-		fmt.Println("  -e string : [REQUIRED] Executable file's path.")
-		fmt.Println("  -z string : [REQUIRED] Target paths to add to zip.")
-		fmt.Println("  -o string : Output file's path.")
-		fmt.Println()
-		fmt.Println("Note:")
-		fmt.Println("  You can set multiple [-z] arguments.")
-	}
-}
-
-// Set flags.
-func (a *Arguments) setFlags() {
-	flag.BoolVar(&a.isHelp, "h", false, "Print usage.")
-	flag.BoolVar(&a.isVersion, "v", false, "Print this help message.")
-	flag.StringVar(&a.exePath, "e", "", "Executable file's path. *REQUIRED")
-	flag.Var(&a.zipPaths, "z", "ZIP target paths. *REQUIRED")
-	flag.StringVar(&a.outPath, "o", "out", "Output file's path.")
-}
-
-// Verify arguments.
-func (a *Arguments) verify() error {
-	// Return if [-h] is specified.
-	if a.isHelp {
-		return nil
-	}
-	// Return if [--version] is specified.
-	if a.isVersion {
-		return nil
-	}
-	// Check [-e] and [-z].
-	if a.exePath == "" {
-		return errors.New("-e option is required.")
-	}
-	if len(a.zipPaths) <= 0 {
-		return errors.New("-z option is required.")
-	}
-	return nil
-}
-
 func main() {
 	// Parse flags
-	args := &Arguments{}
-	args.setUsage()
-	args.setFlags()
+	flag.Usage = usage
+	isHelp := flag.Bool("h", false, "Print usage.")
+	isVersion := flag.Bool("v", false, "Print version.")
 	flag.Parse()
-	// Verify arguments.
-	err := args.verify()
-	if err != nil {
-		fmt.Println(err)
-		fmt.Println()
-		flag.Usage()
-		os.Exit(ERROR_CODE)
-	}
 	// Show usage for [-h]
-	if args.isHelp {
-		flag.Usage()
+	if *isHelp {
+		usage()
 		os.Exit(NORMAL_CODE)
 	}
-	// Show version for [--version].
-	if args.isVersion {
+	// Show version for [-v].
+	if *isVersion {
 		fmt.Println(zgok.Version())
 		os.Exit(NORMAL_CODE)
+	}
+	// Check argument length.
+	args := flag.Args()
+	if len(args) == 0 {
+		usage()
+		os.Exit(ERROR_CODE)
+	}
+	// Switch by command.
+	switch args[0] {
+	case "build":
+		runBuildCommand(args[1:])
+	case "show":
+		runShowCommand(args[1:])
+	default:
+		usage()
+		os.Exit(ERROR_CODE)
+	}
+}
+
+// Usage.
+func usage() {
+	fmt.Println("Usage: zgok [global flags] <command> [flags]")
+	fmt.Println()
+	fmt.Println("commands:")
+	fmt.Println("  build     : Build zgok executable file.")
+	fmt.Println("  list      : List information in zgok executable file.")
+	fmt.Println()
+	fmt.Println("global flags:")
+	fmt.Println("  -h        : Print this help message.")
+	fmt.Println("  -v        : Print version.")
+	fmt.Println()
+	fmt.Println("build command flags:")
+	fmt.Println("  -e string : [REQUIRED] Executable file's path.")
+	fmt.Println("  -z string : [REQUIRED] Target paths to add to zip.")
+	fmt.Println("  -o string : Output file's path.")
+	fmt.Println()
+	fmt.Println("show command flags:")
+	fmt.Println("  -f        : [REQUIRED] Zgok file's path.")
+}
+
+// Run build command.
+func runBuildCommand(args []string) {
+	// Check argument length.
+	if len(args) == 0 {
+		usage()
+		os.Exit(ERROR_CODE)
+	}
+	// Parse flags
+	var (
+		exePath string
+		zipPaths strSlice
+		outPath string
+	)
+	fs := flag.NewFlagSet("build", flag.ExitOnError)
+	fs.StringVar(&exePath, "e", "", "Executable file's path.")
+	fs.Var(&zipPaths, "z", "ZIP target paths.")
+	fs.StringVar(&outPath, "o", "out", "Output file's path.")
+	fs.Parse(args)
+	// Validate arguments.
+	if exePath == "" || len(zipPaths) == 0 || outPath == "" {
+		usage()
+		os.Exit(ERROR_CODE)
 	}
 	// Initialize builder.
 	builder := zgok.NewZgokBuilder()
-	// Set out path.
-	builder.SetOutPath(args.outPath)
-	// Set exe path.
-	err = builder.SetExePath(args.exePath)
+	builder.SetOutPath(outPath)
+	err := builder.SetExePath(exePath)
 	if err != nil {
 		panic(err)
 	}
 	// Add zip paths.
-	for _, zipPath := range args.zipPaths {
+	for _, zipPath := range zipPaths {
 		err = builder.AddZipPath(zipPath)
 		if err != nil {
 			panic(err)
@@ -130,5 +123,41 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Exported %s\n", args.outPath)
+	fmt.Printf("Exported %s\n", outPath)
+}
+
+// Run show command.
+func runShowCommand(args []string) {
+	// Check argument length.
+	if len(args) == 0 {
+		usage()
+		os.Exit(ERROR_CODE)
+	}
+	// Parse flags.
+	var (
+		filePath string
+	)
+	fs := flag.NewFlagSet("show", flag.ExitOnError)
+	fs.StringVar(&filePath, "f", "", "Zgok file's path.")
+	fs.Parse(args)
+	// Check file path.
+	if filePath == "" {
+		flag.Usage()
+		os.Exit(ERROR_CODE)
+	}
+	// Restore zgok file system.
+	zfs, err := zgok.RestoreFileSystem(filePath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(ERROR_CODE)
+	}
+	// Show version.
+	fmt.Println("Signature:")
+	fmt.Println("  " + zfs.String())
+	// Show paths.
+	fmt.Println()
+	fmt.Println("Paths:")
+	for _, path := range zfs.Paths() {
+		fmt.Println("  " + path)
+	}
 }
